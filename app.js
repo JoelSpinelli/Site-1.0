f959570a1d91873d76d9494bdf0f708e70bffa2b
Joel Spinelli <joel.spinelli@hotmail.com>
2019-09-11 18:41:55 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const port = process.env.PORT_APP || 3001;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <link rel="stylesheet" href="/css/style.css">
+    <h1>Joel Spinelli</h1>
+  `);
+});
+
+app.listen(port);
