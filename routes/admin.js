cdd0cfeb58454cdcb0259da31f47208503eb2bf7
JoelSpinelli <joel.spinelli@hotmail.com>
2020-12-08 10:31:42 -0300

    fix

--- routes/admin.js
+++ routes/admin.js
@@ -11,8 +11,8 @@ router.get('/', (req, res) => {
 });
 
 router.get('/ip', (req, res) => {
-  res.json({ip: ( (req.headers['x-forwarded-for'] || '').split(',').pop().trim() ||
-         req.connection.remoteAddress ||
+  res.json({ipv6: (req.headers['x-forwarded-for'] || '').split(',').pop().trim(),
+          ipv4:  ( req.connection.remoteAddress ||
          req.socket.remoteAddress ||
          req.connection.socket.remoteAddress )});
 })
