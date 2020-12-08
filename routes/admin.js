0dde7fbb616a1ea569d9396b4530a3ed6ab68a68
JoelSpinelli <joel.spinelli@hotmail.com>
2020-12-08 16:41:05 -0300

    fix

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const express = require('express');
-const ip = require('../util/ip');
-
 
 const rootDir = require('../util/path');
 const http = require('http');
@@ -13,18 +11,17 @@ router.get('/', (req, res) => {
   // res.sendFile(path.join(rootDir, 'views', 'home.html'));
 });
 
-// const ip_info = get_ip(req, )
+
 
 router.get('/ip', (req, res) => {
   res.json( {
-    ip: ip(req, right_most_proxy=true)
-    // ip: (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || ( req.connection.remoteAddress ||
-    //         req.socket.remoteAddress ||
-    //         req.connection.socket.remoteAddress),
-    // 'x-forwarded-for': (req.headers['x-forwarded-for'] || '').split(',').pop().trim(),
-    // 'connection-remoteAddress': req.connection.remoteAddress || '',
-    // 'socket-remoteAddress': req.socket.remoteAddress || '',
-    // 'connection-socket-remoteAddress': (req.connection.socket || '').remoteAddress
+    ip: (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || ( req.connection.remoteAddress ||
+            req.socket.remoteAddress ||
+            req.connection.socket.remoteAddress),
+    'x-forwarded-for': (req.headers['x-forwarded-for'] || '').split(',').pop().trim(),
+    'connection-remoteAddress': req.connection.remoteAddress || '',
+    'socket-remoteAddress': req.socket.remoteAddress || '',
+    'connection-socket-remoteAddress': (req.connection.socket || '').remoteAddress
     });
 })
 
