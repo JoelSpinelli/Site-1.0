b99026049af61abbde7fb12fd8e8ba9d277654e2
JoelSpinelli <joel.spinelli@hotmail.com>
2020-12-08 10:48:02 -0300

    change

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
 
 router.get('/ip', (req, res) => {
   res.json({ipv6: (req.headers['x-forwarded-for'] || '').split(',').pop().trim(),
-          ipv4:  ( req.connection.remoteAddress ||
+          ipv4:  ( req.connection.localAddress ||
          req.socket.remoteAddress ||
          req.connection.socket.remoteAddress )});
 })
