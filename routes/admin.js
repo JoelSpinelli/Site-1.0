f740fa08b83ca42f6ea99a2a18811ff16c2dd142
JoelSpinelli <joel.spinelli@hotmail.com>
2020-12-08 16:11:11 -0300

    teste

--- routes/admin.js
+++ routes/admin.js
@@ -20,6 +20,7 @@ router.get('/ip', (req, res) => {
             req.socket.remoteAddress ||
             req.connection.socket.remoteAddress),
     'x-forwarded-for': (req.headers['x-forwarded-for'] || '').split(',').pop().trim(),
+    'forwareded': req.headers['forwarded'],
     'connection-remoteAddress': req.connection.remoteAddress || '',
     'socket-remoteAddress': req.socket.remoteAddress || '',
     'connection-socket-remoteAddress': (req.connection.socket || '').remoteAddress
