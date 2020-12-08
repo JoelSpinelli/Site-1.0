42539b1056c7639c8cad4b1339232288617adc13
JoelSpinelli <joel.spinelli@hotmail.com>
2020-12-08 12:13:28 -0300

    teste

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const requestIp = require('request-ip');
 
 const rootDir = require('../util/path');
 const http = require('http');
@@ -15,17 +16,8 @@ router.get('/ip', (req, res) => {
   res.json(
     {ip: (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || ( req.connection.remoteAddress ||
             req.socket.remoteAddress ||
-            req.connection.socket.remoteAddress ),
-      ipExpress: req.ip,
-      'x-real-ip': req.headers['x-real-ip'],
-      'x-client-ip': req.headers['x-client-ip'],
-      'cf-connecting-ip': req.headers['cf-connecting-ip'],
-      'fastly-client-ip': req.headers['fastly-client-ip'],
-      'true-client-ip': req.headers['true-client-ip'],
-      'x-cluster-client-ip': req.headers['x-cluster-client-ip'],
-      'x-forwarded': req.headers['x-forwarded'],
-      'forwarded-for': req.headers['forwarded-for'],
-      'x-real-i': req.headers['x-real-ip']
+            req.connection.socket.remoteAddress),
+    teste: requestIp(req)
     });
 })
 
