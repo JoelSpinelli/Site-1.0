99315f7f62cc8e8d14e5bc8628a710675d1cc08c
JoelSpinelli <joel.spinelli@hotmail.com>
2020-12-08 12:07:25 -0300

    teste

--- routes/admin.js
+++ routes/admin.js
@@ -12,11 +12,23 @@ router.get('/', (req, res) => {
 });
 
 router.get('/ip', (req, res) => {
-  res.json({ip: (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || ( req.connection.remoteAddress ||
-         req.socket.remoteAddress ||
-         req.connection.socket.remoteAddress ),
-        ipTeste: req.headers['x-real-ip']});
+  res.json(
+    {ip: (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || ( req.connection.remoteAddress ||
+            req.socket.remoteAddress ||
+            req.connection.socket.remoteAddress ),
+      ipExpress: req.ip,
+      'x-real-ip': req.headers['x-real-ip'],
+      'x-client-ip': req.headers['x-client-ip'],
+      'cf-connecting-ip': req.headers['cf-connecting-ip'],
+      'fastly-client-ip': req.headers['fastly-client-ip'],
+      'true-client-ip': req.headers['true-client-ip'],
+      'x-cluster-client-ip': req.headers['x-cluster-client-ip'],
+      'x-forwarded': req.headers['x-forwarded'],
+      'forwarded-for': req.headers['forwarded-for'],
+      'x-real-i': req.headers['x-real-ip']
+    });
 })
+
 
 
 
